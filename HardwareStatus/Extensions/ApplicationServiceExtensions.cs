b7b03dbe7a26888cc7264b6b45b8a49f33669d28
matchodura <69363257+matchodura@users.noreply.github.com>
2022-04-21 19:10:46 +0200

    Add base for HardwareStatus service

---
+++ HardwareStatus/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,54 @@
+﻿using HardwareStatus.API.Profiles;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using System;
+
+namespace HardwareStatus.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddApiVersioning(options =>
+            {
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.ReportApiVersions = true;
+            });
+
+            services.AddCors(options => {
+                options.AddPolicy("CorsPolicy", builder => builder
+                 .WithOrigins("http://localhost:4200/")
+                 .SetIsOriginAllowed((host) => true)
+                 .AllowAnyMethod()
+                 .AllowAnyHeader()
+                 .AllowCredentials());
+            });
+
+            services.AddAutoMapper(typeof(AutoMapperHardwareProfile).Assembly);
+
+            services.AddSingleton<Serilog.ILogger>(CreateSerilogLogger(config));
+
+            return services;
+        }
+
+        private static Serilog.ILogger CreateSerilogLogger(IConfiguration configuration)
+        {
+            string machineName = Environment.MachineName;
+            var seqServerUrl = configuration["Serilog:SeqServerUrl"];
+
+            return new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .Enrich.FromLogContext()
+                .Enrich.WithProperty("_service", "hardware-status-api")
+                .Enrich.WithProperty("_machine", machineName)
+                .WriteTo.Seq(string.IsNullOrWhiteSpace(seqServerUrl) ? "http://seq" : seqServerUrl)
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+        }
+
+    }
+}
---
+++ HardwareStatus/Profiles/AutoMapperHardwareProfile.cs
@@ -0,0 +1,8 @@
+﻿using AutoMapper;
+
+namespace HardwareStatus.API.Profiles
+{
+    public class AutoMapperHardwareProfile : Profile
+    {
+    }
+}
